777caa7d4f2426c59d6785d32d101f9fe47a02c0
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-08-04 11:03:01 -0300

    Strings Trim

---
+++ 07-funcao-nativas.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções Nativas </title>
+</head>
+<body>
+
+<h1>Exemplo de funções nativas</h1>
+<hr>
+
+<h2>Strings</h2>
+<h3>trim()</h3>
+<?php
+/* Função que remove espaço antes e depois de strings(caractere). */
+$nome = "     Vinícius me deve 80 paçocas      ";
+$nomeSemEspaco = trim($nome);
+?>
+
+<pre> <?=var_dump($nome)?></pre>
+<pre> <?=var_dump($nomeSemEspaco)?> </pre>
+
+
+<hr>
+<h2>Numéricas</h2>
+
+
+<h2>Arrays</h2>
+
+
+<h2>Filtro</h2>
+
+
+<h2>Segurança</h2>
+    
+</body>
+</html>

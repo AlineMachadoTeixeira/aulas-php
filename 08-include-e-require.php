f168868bff022ab347a25357babf12f801968ae5
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-08-07 10:48:12 -0300

    Include e Require

--- 08-include-e-require.php
+++ 08-include-e-require.php
@@ -1,7 +1,14 @@
 <?php
 /* Fazendo a inclusão de um arquivo de recursos */
 /* Está trazaendo tudo que está na pasta recursos.php */
-include "recursos.php";
+require "recursos.php";
+
+/* Include OU require 
+A diferença entre eles é na forma como lidam no caso de acontecer algum erro na inclusão
+
+Erros de carregamento com include, dão alertas (warnings) mas NÃO PARAM completamente a aplicação
+
+Erros de carregamento com require, dão erro fatal (fatal error), PARAM completamente a aplicação.*/
 
 ?>
 <!DOCTYPE html>
@@ -18,6 +25,32 @@ include "recursos.php";
 
 <h2><?=ESCOLA?></h2>
 <p>Estamos fazendo o curso de <?=$curso?></p>
-    
+
+<!-- Monte uma lista não ordenada contendo cada tecnologias -->
+    <ul>
+        <li><?=$tecnologias[0]?></li>
+        <li><?=$tecnologias[1]?></li>
+        <li><?=$tecnologias[2]?></li>
+        <li><?=$tecnologias[3]?></li>
+    </ul>
+
+<!-- Monte uma lista não ordenada contendo cada tecnologias versão professor-->
+<ul>
+<?php foreach($tecnologias as $tecnologia){?>
+     <li><?=$tecnologia?></li>
+<?php
+}
+?>   
+</ul>
+
+<p>O aluno Fulano  tem 20 anos e é <?=verificarIdade(20)?> idade</p>
+
+<hr>
+
+<article>
+    <h2>Título qualquer...</h2>
+    <?php include "texto.php"?>
+</article>
+  
 </body>
 </html>
---
+++ texto.php
@@ -0,0 +1,7 @@
+<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit dolore autem, libero ratione voluptas in possimus placeat, nesciunt minus optio dolores rem culpa, totam hic quis quam illum voluptates non.</p>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit magni tempore quibusdam ipsa debitis dicta sapiente beatae ad vitae dolore.</p>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, eaque.</p>
+
+<?php include "lista.html"?>

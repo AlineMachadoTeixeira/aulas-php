6d1071c5e1075934873b232ba5866848e4e1c355
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-08-07 11:58:11 -0300

    Exercício 06

---
+++ 09-site/contato.php
@@ -0,0 +1,12 @@
+<?php
+require "includes/header.php"
+?>
+
+<h2>Nosso Contatos</h2>
+<p>Tel (11)99999-999</p>
+<p>End: Rua Lorem ipsum dolor sit amet. </p>
+
+        
+<?php
+require "includes/footer.php";
+?>     
--- 09-site/includes/footer.php
+++ 09-site/includes/footer.php
@@ -2,6 +2,22 @@
 
     <footer>
         <p>Site criado para o curso <b>Téc. Informatica para Internet</b></p>
+        
+
+        <?php
+        //Variavel HORAS
+        $hora = date("H:i:s");  //h:horas - i:minutos - s:segundos
+        //Data Para Constante faz assim:
+        define("DATA", date("d/m/Y"));
+        echo DATA;
+
+        echo "<br>"; //para mostrar a hora
+        echo "Hora: $hora";
+        echo "<br><br>";
+        
+        ?>
+
+        
     </footer>
 
 

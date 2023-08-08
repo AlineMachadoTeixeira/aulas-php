5098e4d3e9e6c5f7e10bd9873ace8b3b03f2caa7
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-08-08 11:13:51 -0300

    Verificar se os campos nome e e-mail estão vazios

--- processa-post.php
+++ processa-post.php
@@ -11,18 +11,36 @@
     <p>Receber e processar dados via <b>Post</b></p>
 
     <?php
+    /* Verificar se os campos nome e e-mail estão vazios 
+    
+    barrinha ||    é operador    OU  OR 
+             &&    é operador    E   AND 
+             !     é operador    Não  NOT */
+    if( empty ($_POST ["nome"]) || empty($_POST ["email"]) ){
+    ?>    
+        <p>Você deve preencher nome e e-mail</p>";
+        <p> <a> href \"10-formulario.html\"</a></p>";
+
+   <?php 
+   } else{
+    
     $nome = $_POST["nome"];
     $email = $_POST["email"];
     $mensagem = $_POST["mensagem"];
-    ?>
-    
+    ?>  
+
 
     <h2>Dados:</h2>
+
     <ul>
         <li>Nome: <?=$nome?> </li>
         <li>E-mail: <?=$email?> </li>
         <li>Mensagem:<?=$mensagem?> </li>        
     </ul>
+   <?php
+   }
+   ?>
+  
     
 </body>
 </html>

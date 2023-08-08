9d0b0ace77653c61a80eb063c41f78b4f7d884f7
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-08-08 09:02:39 -0300

    Identificar qual página solução 1

--- 09-site/index.php
+++ 09-site/index.php
@@ -4,8 +4,8 @@ require "includes/header.php"
 ?>
 
         <h2>Bem-vindo ao site exemplo</h2>
-        <p>Este é a primeira página do nosso site</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic, sit, voluptate reiciendis provident pariatur, dolorum reprehenderit soluta blanditiis vitae possimus et quas sed suscipit qui doloremque perferendis sint deleniti! Ipsum consectetur deserunt laborum inventore aliquam hic porro voluptatem quibusdam modi.</p>
+        <p>Este é a primeira página do nosso site </p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic, sit, voluptate reiciendis provident pariatur, dolorum reprehenderit soluta blanditiis 0000 vitae possimus et quas sed suscipit qui doloremque perferendis sint deleniti! Ipsum consectetur deserunt laborum inventore aliquam hic porro voluptatem quibusdam modi.</p>
 
 <?php
 require "includes/footer.php";

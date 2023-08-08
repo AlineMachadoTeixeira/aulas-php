cc30386181350c890e1a6d28efe76b0a69ef38d4
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-08-08 10:45:25 -0300

    Processo Post

---
+++ processa-post.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Processo Post</title>
+</head>
+<body>
+    <h1>Exemplo usando Post</h1>
+    <hr>
+    <p>Receber e processar dados via <b>Post</b></p>
+
+    <?php
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $mensagem = $_POST["mensagem"];
+    ?>
+    
+
+    <h2>Dados:</h2>
+    <ul>
+        <li>Nome: <?=$nome?> </li>
+        <li>E-mail: <?=$email?> </li>
+        <li>Mensagem:<?=$mensagem?> </li>        
+    </ul>
+    
+</body>
+</html>
